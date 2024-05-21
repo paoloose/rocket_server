8fab89667dc1cf40bffb533a7e0fdf8e7a076362
Luis Calle <luedu1103@gmail.com>
2024-05-20 22:15:50 -0500

    i think its working ?

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,8 @@ use rocket::http::Status;
 use rocket::response::Redirect;
 use rocket::serde::json::{json, Value};
 
-use openai_api_rs::v1::api::Client;
-use openai_api_rs::v1::chat_completion::{self, ChatCompletionRequest};
-use openai_api_rs::v1::common::GPT4;
-use dotenv::dotenv;
-
-// pub mod ia;
-// use crate::ia::open_ai::chat_ai;
+pub mod ia;
+use crate::ia::open_ai::chat_ai;
 
 #[macro_use] 
 extern crate rocket;
@@ -41,8 +36,7 @@ fn google_keep_desktop_api(_platform: &str, _current_version: &str, msg: Option<
 
 #[get("/chat_with_ai?<msg>")]
 fn open_ai_chat(msg: String) -> Result<Value, Status> {
-    // let string = chat_ai::chat(msg).unwrap_or(Some("carajo mierda".to_string()));
-    let string = chat(msg).unwrap_or(Some("carajo mierda".to_string()));
+    let string = chat_ai::chat(msg).unwrap_or(Some("carajo mierda".to_string()));
     
     Ok(json!({
         "notes": string
@@ -69,28 +63,4 @@ async fn main() -> shuttle_rocket::ShuttleRocket {
         .mount(GPTHOLA, routes![open_ai_chat]);
 
     Ok(rocket.into())
-}
-
-fn chat(message: String) -> Result<Option<String>, Box<dyn std::error::Error>> {
-    dotenv().ok();
-    let api_token = std::env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY must be set.");
-    let client = Client::new(api_token);
-
-    println!("{}", &message);
-    
-    let req = ChatCompletionRequest::new(
-        GPT4.to_string(),
-        vec![chat_completion::ChatCompletionMessage {
-            role: chat_completion::MessageRole::user,
-            content: chat_completion::Content::Text(message),
-            name: None,
-        }],
-    );
-
-    let result = client.chat_completion(req)?;
-    let string = &result.choices[0].message.content;
-    // println!("Content: {:?}", result.choices[0].message.content);
-    // println!("Response Headers: {:?}", result.headers);
-
-    Ok(string.clone())
 }
